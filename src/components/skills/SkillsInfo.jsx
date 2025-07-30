a78a4815a17aca6bf265aaaa5c3fee64bd482cae
Maksim <Daymansolvatore88@gmail.com>
2025-07-30 22:34:45 +0300

    v 0.4

--- src/components/skills/SkillsInfo.jsx
+++ src/components/skills/SkillsInfo.jsx
@@ -3,6 +3,7 @@ import Awords from "./Awords";
 import Education from "./Education";
 import Skills from "./Skills";
 import "./Skills.css";
+import WorkExperience from "./WorkExperience";
 
 const SkillsInfo = () => {
   return (
@@ -23,50 +24,7 @@ const SkillsInfo = () => {
             <Skills />
             <Awords />
           </div>
-          <div className="education work-exp">
-            <h3 className="work-exp-title">Work & Experience</h3>
-            <div className="skills-info">
-              <div className="experience-card">
-                <div className="upper">
-                  <h3>Sr. Graphic Designer</h3>
-                  <h5>Part Time | Office</h5>
-                  <span>2020 - 2021</span>
-                </div>
-                <div className="hr"></div>
-                <h4 className="label">Avada Theme.</h4>
-                <p>
-                  I design and develop services for customers of all sizes,
-                  specializing in creating stylish, modern websites
-                </p>
-              </div>
-              <div className="experience-card">
-                <div className="upper">
-                  <h3>Cr. Web Developer</h3>
-                  <h5>Full Time | InHouse</h5>
-                  <span>2019 - 2020</span>
-                </div>
-                <div className="hr"></div>
-                <h4 className="label">ib-themes ltd.</h4>
-                <p>
-                  I design and develop services for customers of all sizes,
-                  specializing in creating stylish, modern websites
-                </p>
-              </div>
-              <div className="experience-card">
-                <div className="upper">
-                  <h3>Jr. Web Developer</h3>
-                  <h5>Full Time | Remote</h5>
-                  <span>2018 - 2019</span>
-                </div>
-                <div className="hr"></div>
-                <h4 className="label">Creative Gigs.</h4>
-                <p>
-                  I design and develop services for customers of all sizes,
-                  specializing in creating stylish, modern websites
-                </p>
-              </div>
-            </div>
-          </div>
+          <WorkExperience />
         </div>
       </div>
     </section>
---
+++ src/data/workExperience.js
@@ -0,0 +1,26 @@
+export const workExperience = [
+  {
+    title: "Sr. Graphic Designer",
+    type: "Part Time | Office",
+    date: "2020 - 2021",
+    company: "Avada Theme",
+    description:
+      "I design and develop services for customers of all sizes, specializing in creating stylish, modern websites",
+  },
+  {
+    title: "Cr. Web Developer",
+    type: "Full Time | InHouse",
+    date: "2019 - 2020",
+    company: "ib-themes ltd.",
+    description:
+      "I design and develop services for customers of all sizes, specializing in creating stylish, modern websites",
+  },
+  {
+    title: "Jr. Web Developer",
+    type: "Full Time | Remote",
+    date: "2018 - 2019",
+    company: "Creative Gigs",
+    description:
+      "I design and develop services for customers of all sizes, specializing in creating stylish, modern websites",
+  },
+];
